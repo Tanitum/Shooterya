18ff9924c175e430b31b3ccc9172a7c45e91fd1b
Sergey Bogatov <moonygamesstudio@gmail.com>
2023-05-14 02:56:16 +0300

    Started to make multiplayer

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,44 +1,64 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] private float speed = 3f;//скорость движения
-    [SerializeField] private int health = 5;//кол-во здоровья
-    [SerializeField] private float jumpForce = 15f;//сила прыжка
-	[SerializeField] private AudioSource enemyDamageSound;//звук получения урона
-	
+    [SerializeField] private float _speed = 3f;//скорость движения
+    [SerializeField] private float _jumpForce = 15f;//сила прыжка
+
+	[SerializeField] private AudioSource _audioSource;//звук получения урона
+
+	private delegate void DeathEventHandler();
+	private event DeathEventHandler OnDeath;
+
+    [SerializeField]  private int _health;
+	private int health
+	{
+		get { return _health; }
+
+		set
+		{
+			_health = value;
+
+			if (health <= 0)
+				OnDeath?.Invoke();
+		}
+	}
+
     private Rigidbody2D rb;
     private SpriteRenderer sprite;
     private Animator anim;
 	
     private void Awake()
     {
+		OnDeath += Death;
+
         rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
         sprite = GetComponentInChildren<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
-	
-	private void Update()
-	{
-		if(health <= 0)
-		{
-			Destroy(gameObject);
-		}
-	}
-	
+
 	public void TakeDamage(int damage)
 	{
 		health -= damage;
-		enemyDamageSound.Play();
+
+        _audioSource.Play();
 	}
 	
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
-		if (collision.gameObject == Hero.Instance.gameObject)
+		if (collision.gameObject.tag == "Player")
 		{
-			Hero.Instance.GetDamage();
+			Player.Singleton.GetDamage();
 		}
+	}
+
+	private void Death()
+	{
+		Destroy(gameObject);
+	}
+
+    private void OnDestroy()
+	{
+		OnDeath -= Death;
 	}
 }
