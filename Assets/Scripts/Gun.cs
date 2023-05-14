f9d615889e4fb19512a72e06fc8d82979dd5d666
Tanitum <komlev.ra@phystech.edu>
2023-05-14 20:01:04 +0300

    test multiplayer
    test multiplayer

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,29 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using Unity.Netcode;
 
-public class Gun : MonoBehaviour {
-
+public class Gun : NetworkBehaviour
+{
     public float offset; // параметр для корректировки поворота пушки
     public GameObject projectile; // внешний вид оружия
     public Transform shotPoint; // место вылета снаряда
-	[SerializeField] private AudioSource shotSound;// звук выстрела
+	[SerializeField] private AudioSource _shotSound;// звук выстрела
+
+	private SpriteRenderer sprite;
 
     private float timeBtwShots; // время между выстрелами
     public float startTimeBtwShots;
 
-    void Update()
+    private void Awake()
+    {
+		sprite = GetComponent<SpriteRenderer>();
+
+		_shotSound = GetComponent<AudioSource>();
+    }
+
+    private void Update()
     {
 		Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+
 		float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
 		transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
+
+		if (difference.x < 0)
+			sprite.flipY = true;
+		else
+			sprite.flipY = false;
 
 		if (timeBtwShots <= 0)
 		{
 			if (Input.GetMouseButton(0))
 			{
-				shotSound.Play();
-				Instantiate(projectile, shotPoint.position, transform.rotation);
+				_shotSound.Play();
+
+				GameObject spawnedObject = Instantiate(projectile, shotPoint.position, transform.rotation);
+				spawnedObject.GetComponent<NetworkObject>().Spawn(true);
+
 				timeBtwShots = startTimeBtwShots;
 			}
 		}
